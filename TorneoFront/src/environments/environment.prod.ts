c6f35bc441a608a56e047a3ccf305932432f0b36
difeorqui <dfoq76@hotmail.com>
2021-08-02 14:54:21 -0500

    Se actualiza el usaurio de conexión en producción con MySQL

--- TorneoFront/src/environments/environment.prod.ts
+++ TorneoFront/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "http://localhost:3000",
+  apiUrl: "http://localhost:4000",
   login: "/api/login",
   registrarUsario: "/api/usuario",
   equipo: "/api/equipo",
